29941bbe22e4e2120286559c97d4319096310718
fabiii-20 <fabishereef@gmail.com>
2024-08-11 12:41:00 +0530

    ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,14 @@ require('dotenv').config();
 
 const app = express();
 
-const corsOptions = {
-  origin: 'http://localhost:5173/', // Replace with your frontend URL
-  methods: 'GET,POST',
-  allowedHeaders: 'Content-Type',
-};
+// const corsOptions = {
+//   origin: 'http://localhost:5173/', // Replace with your frontend URL
+//   methods: 'GET,POST',
+//   allowedHeaders: 'Content-Type',
+// };
 
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -23,6 +23,7 @@ mongoose.connect(process.env.MONGO_URI, {
   .catch((err) => console.log(err));
 
 app.use('/api', otpRoutes);
-
+// app.get("/health",async (req, res) => {
+//   return res.status(200).send('OTP verified')})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
